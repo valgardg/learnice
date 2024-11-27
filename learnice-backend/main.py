7c0bd4b3d2b146109bc925b0595241a19c34830a
valgardg <valgardo20@ru.is>
2024-11-27 15:32:13 -0500

    removed comments and updated function names to make more sense

--- learnice-backend/main.py
+++ learnice-backend/main.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 
 from check_grammar import check_sentence_grammar
 from tag_sentence import predict_tags
-from translate_word import translate_english_to_icelandic
+from translate_word import translate_english_sentence_to_icelandic
 from classify_sentence import classify_sentence_language
 
 app = FastAPI()
@@ -29,7 +29,7 @@ def pos_tag_sentence(sentence: str):
 
         # Step 2: Translate if needed and process the sentence
         if sentence_lang == "English-Latin1":
-            translated_icelandic_sentence = translate_english_to_icelandic(sentence)
+            translated_icelandic_sentence = translate_english_sentence_to_icelandic(sentence)
             tagged_sentence = predict_tags(translated_icelandic_sentence)
             grammar_suggestions = [] # assume that translation doesn't contain spelling or grammar mistakes
         else:
--- learnice-backend/translate_word.py
+++ learnice-backend/translate_word.py
@@ -18,50 +18,29 @@ translate = boto3.client(
     region_name=aws_region
 )
 
-def translate_icelandic_to_english(word):
-    """
-    Translates an Icelandic word to English using AWS Translate.
-
-    Parameters:
-    word (str): The Icelandic word to translate.
-
-    Returns:
-    str: The translated English word.
-    """
-    # Create a Translate client
-    translate = boto3.client('translate', region_name='us-east-2')  # Adjust region if needed
+def translate_icelandic_word_to_english(word):
+    translate = boto3.client('translate', region_name='us-east-2')
 
     try:
         # Call AWS Translate
         response = translate.translate_text(
             Text=word,
-            SourceLanguageCode='is',  # Icelandic language code
-            TargetLanguageCode='en'  # English language code
+            SourceLanguageCode='is',
+            TargetLanguageCode='en'
         )
         return response['TranslatedText']
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
     
-def translate_english_to_icelandic(sentence):
-    """
-    Translates an English sentence to Icelandic using AWS Translate.
-
-    Parameters:
-    sentence (str): The English sentence to translate.
-
-    Returns:
-    str: The translated Icelandic sentence.
-    """
-    # Create a Translate client
-    translate = boto3.client('translate', region_name='us-east-1')  # Adjust region if needed
+def translate_english_sentence_to_icelandic(sentence):
+    translate = boto3.client('translate', region_name='us-east-2')
 
     try:
-        # Call AWS Translate
         response = translate.translate_text(
             Text=sentence,
-            SourceLanguageCode='en',  # English language code
-            TargetLanguageCode='is'  # Icelandic language code
+            SourceLanguageCode='en',
+            TargetLanguageCode='is'
         )
         return response['TranslatedText']
     except Exception as e:
